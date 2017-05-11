2a8c2547b93b86bedb4ec05138f5e54bb33b0918
Zachary De Maria <s171558@ad.aie.edu>
2017-05-11 14:36:20 +0930

    first content commit

---
+++ Ocarina Computer Controller/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Ocarina_Computer_Controller
+{
+    class Main
+    {
+        WaveIn micIn;
+
+        public Main()
+        {
+            micIn = new WaveIn();
+        }
+
+        public void Run()
+        {
+            micIn.DataAvailable += new EventHandler<WaveInEventArgs>(OnDataRecieved);
+            micIn.StartRecording();
+        }
+
+        void OnDataRecieved(object sender, WaveInEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ocarina Computer Controller/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Ocarina_Computer_Controller
+{
+    class Program
+    {
+        static Main main = new Main();
+        static void Main(string[] args)
+        {
+            main.Run();
+        }
+    }
+}
